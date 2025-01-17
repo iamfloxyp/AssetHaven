9c295888e5c6f9179c4c25fbc90bfd128b03ec0e
onyeka <onye163@outlook.com>
2025-01-17 04:18:08 -0500

    Add backend server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Backend is running!');
+    res.send('Backend server is running!');
 });
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
