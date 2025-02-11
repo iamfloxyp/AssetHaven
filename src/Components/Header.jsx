61343eab781417cd616b31c05cb6abf59845056f
onyeka ossai <onye163@outlook.com>
2025-02-11 17:07:46 +0000

    Fix: Add a new image

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 import  { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars,faTimes } from "@fortawesome/free-solid-svg-icons";
-import Shield from '../images/Shield.png'
+import { faBars,faTimes } from "@fortawesome/free-solid-svg-icons"
+import Shield from "../images/Shield.png"
 
 
 const Header = () => {
