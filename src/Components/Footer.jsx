70fb7ef02d7eed3449bb63d4b79318c01f7157a8
onyeka ossai <onye163@outlook.com>
2025-02-12 15:41:26 +0000

    Fix:Add the right location

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
         {/* Contact Section */}
         <div className="footer-contact">
           <h3>Get in Touch</h3>
-          <p>123 Financial District, London, UK</p>
+          <p>40 Rayburn Way , Cheetham Hill,Manchester, UK</p>
           <p>Email: <a href="mailto:support@assethaven.com">support@assethaven.com</a></p>
           <Link to="/contact" className="contact-btn">Contact Us</Link>
         </div>
